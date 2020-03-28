621061e86ddf79d94dc7e782afebc1c79a6218d9
az900970 <az900970@users.noreply.github.com>
2020-03-28 15:57:53 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -65,8 +65,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     int count = 0;
      RGBTRIPLE *temp = calloc (width, sizeof (RGBTRIPLE) ) ;
      if (temp == NULL)
-     {printf ("ERROR");
-      return ;
+     {printf ("ERROR: Insufficient memory");
+      return;
      }
     for ( int x = 0; x < height; x++)
       {
@@ -82,8 +82,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
               }
             }
         }
-
-
       }
     free (temp);
     return;
@@ -92,5 +90,102 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for ( int x = 0; x < height - 1; x ++ )
+    {
+       for (int y = 0; y < width - 1; y++ )
+       {
+          if ( x == 0 && y == 0 )  // Top Left Corner element
+          {
+              image[x][y].rgbtRed = round ( image[x][y].rgbtRed + image[x][y + 1].rgbtRed + image[x + 1 ][y].rgbtRed
+              + image[x + 1][y + 1].rgbtRed ) / 4.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y].rgbtBlue + image[x][y + 1].rgbtBlue + image[x + 1 ][y].rgbtBlue
+              + image[x + 1][y + 1].rgbtBlue ) / 4.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y].rgbtGreen + image[x][y + 1].rgbtGreen + image[x + 1 ][y].rgbtGreen
+              + image[x + 1][y + 1].rgbtGreen ) / 4.0 ;
+          }
+
+          if ( x == 0 && y != 0 ) // Top Edge of picture
+          {
+              image[x][y].rgbtRed = round ( image[x][y - 1].rgbtRed +  image[x][y].rgbtRed + image[x][y + 1].rgbtRed
+              + image[x + 1 ][y - 1].rgbtRed + image[x + 1][y].rgbtRed + image[x + 1][y + 1].rgbtRed ) / 6.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y - 1].rgbtBlue +  image[x][y].rgbtBlue + image[x][y + 1].rgbtBlue +
+              image[x + 1 ][y - 1].rgbtBlue + image[x + 1][y].rgbtBlue + image[x + 1][y + 1].rgbtBlue ) / 6.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y - 1].rgbtGreen +  image[x][y].rgbtGreen + image[x][y + 1].rgbtGreen
+              + image[x + 1 ][y - 1].rgbtGreen + image[x + 1 ][y].rgbtGreen + image[x + 1][y + 1].rgbtGreen ) / 6.0 ;
+          }
+
+          if ( x == 0 && y == width - 1 )  // Top Right Corner Element
+          {
+              image[x][y].rgbtRed = round ( image[x][y].rgbtRed + image[x][y - 1].rgbtRed + image[x + 1 ][y].rgbtRed
+              + image[x + 1][y - 1].rgbtRed ) / 4.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y].rgbtBlue + image[x][y - 1].rgbtBlue + image[x + 1 ][y].rgbtBlue
+              + image[x + 1][y - 1].rgbtBlue ) / 4.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y].rgbtGreen + image[x][y - 1].rgbtGreen + image[x + 1 ][y].rgbtGreen
+              + image[x + 1][y - 1].rgbtGreen ) / 4.0 ;
+          }
+
+          if ( x != 0 && y == 0 )  // Left edge of picture
+          {
+              image[x][y].rgbtRed = round ( image[x - 1][y].rgbtRed +  image[x][y].rgbtRed + image[x + 1][y].rgbtRed
+              + image[x - 1][y + 1].rgbtRed + image[x][y + 1].rgbtRed + image[x + 1][y + 1].rgbtRed ) / 6.0 ;
+              image[x][y].rgbtBlue = round ( image[x - 1][y].rgbtBlue +  image[x][y].rgbtBlue + image[x + 1][y].rgbtBlue
+              + image[x - 1][y + 1].rgbtBlue + image[x][y + 1].rgbtBlue + image[x + 1][y + 1].rgbtBlue ) / 6.0 ;
+              image[x][y].rgbtGreen = round ( image[x - 1][y].rgbtGreen +  image[x][y].rgbtGreen + image[x + 1][y].rgbtGreen
+              + image[x - 1][y + 1].rgbtGreen + image[x][y + 1].rgbtGreen + image[x + 1][y + 1].rgbtGreen ) / 6.0 ; ;
+          }
+
+           if ( x != 0 && y == width - 1 )  // Right edge of picture
+          {
+              image[x][y].rgbtRed = round ( image[x - 1][y].rgbtRed +  image[x][y].rgbtRed + image[x + 1][y].rgbtRed
+              + image[x - 1][y - 1].rgbtRed + image[x][y - 1].rgbtRed + image[x + 1][y - 1].rgbtRed ) / 6.0 ;
+              image[x][y].rgbtBlue = round ( image[x - 1][y].rgbtBlue +  image[x][y].rgbtBlue + image[x + 1][y].rgbtBlue
+              + image[x - 1][y - 1].rgbtBlue + image[x][y - 1].rgbtBlue + image[x + 1][y - 1].rgbtBlue ) / 6.0 ;
+              image[x][y].rgbtGreen = round ( image[x - 1][y].rgbtGreen +  image[x][y].rgbtGreen + image[x + 1][y].rgbtGreen
+              + image[x - 1][y - 1].rgbtGreen + image[x][y - 1].rgbtGreen + image[x + 1][y - 1].rgbtGreen ) / 6.0 ; ;
+          }
+
+           if ( x == height - 1 && y == 0 )  // Bottom Left Corner element
+          {
+              image[x][y].rgbtRed = round ( image[x][y].rgbtRed + image[x][y + 1].rgbtRed + image[x - 1 ][y].rgbtRed
+              + image[x - 1][y + 1].rgbtRed ) / 4.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y].rgbtBlue + image[x][y + 1].rgbtBlue + image[x - 1 ][y].rgbtBlue
+              + image[x - 1][y + 1].rgbtBlue ) / 4.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y].rgbtGreen + image[x][y + 1].rgbtGreen + image[x - 1 ][y].rgbtGreen
+              + image[x - 1][y + 1].rgbtGreen ) / 4.0 ;
+          }
+
+          if ( x == height - 1 && y != 0 ) // Bottom Edge of picture
+          {
+              image[x][y].rgbtRed = round ( image[x][y - 1].rgbtRed +  image[x][y].rgbtRed + image[x][y + 1].rgbtRed
+              + image[x - 1 ][y - 1].rgbtRed + image[x - 1][y].rgbtRed + image[x - 1][y + 1].rgbtRed ) / 6.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y - 1].rgbtBlue +  image[x][y].rgbtBlue + image[x][y + 1].rgbtBlue +
+              image[x - 1 ][y - 1].rgbtBlue + image[x - 1][y].rgbtBlue  + image[x - 1][y + 1].rgbtBlue ) / 6.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y - 1].rgbtGreen +  image[x][y].rgbtGreen + image[x][y + 1].rgbtGreen
+              + image[x - 1 ][y - 1].rgbtGreen + image[x - 1][y].rgbtGreen + image[x - 1][y + 1].rgbtGreen ) / 6.0 ;
+          }
+
+           if ( x == height - 1 && y == width - 1 )  // Bottom Right Corner Element
+          {
+              image[x][y].rgbtRed = round ( image[x][y - 1].rgbtRed + image[x][y].rgbtRed + image[x - 1 ][y].rgbtRed
+              + image[x - 1][y - 1].rgbtRed ) / 4.0 ;
+              image[x][y].rgbtBlue = round ( image[x][y - 1].rgbtBlue + image[x][y].rgbtBlue + image[x - 1 ][y].rgbtBlue
+              + image[x - 1][y - 1].rgbtBlue ) / 4.0 ;
+              image[x][y].rgbtGreen = round ( image[x][y - 1].rgbtGreen + image[x][y].rgbtGreen + image[x - 1 ][y].rgbtGreen
+              + image[x - 1][y - 1].rgbtGreen ) / 4.0 ;
+          }
+           if ( x != 0 && x < height && y != 0 && y < width  )
+          {
+              image[x][y].rgbtRed = round ( image[x - 1][y - 1].rgbtRed +  image[x - 1][y].rgbtRed + image[x - 1][y + 1].rgbtRed
+              + image[x][y - 1].rgbtRed + image[x][y].rgbtRed + image[x][y + 1].rgbtRed
+              + image[x + 1][y - 1].rgbtRed + image[x + 1][y].rgbtRed + image[x + 1][y + 1].rgbtRed) / 9.0 ;
+              image[x][y].rgbtBlue = round ( image[x - 1][y - 1].rgbtBlue +  image[x - 1][y].rgbtBlue + image[x - 1][y + 1].rgbtBlue
+              + image[x][y - 1].rgbtBlue + image[x][y].rgbtBlue + image[x][y + 1].rgbtBlue
+              + image[x + 1][y - 1].rgbtBlue + image[x + 1][y].rgbtBlue + image[x + 1][y + 1].rgbtBlue) / 9.0 ;
+              image[x][y].rgbtGreen = round ( image[x - 1][y - 1].rgbtGreen +  image[x - 1][y].rgbtGreen + image[x - 1][y + 1].rgbtGreen
+              + image[x][y - 1].rgbtGreen + image[x][y].rgbtGreen + image[x][y + 1].rgbtGreen
+              + image[x + 1][y - 1].rgbtGreen + image[x + 1][y].rgbtGreen + image[x + 1][y + 1].rgbtGreen) / 9.0 ;
+          }
+       }
+    }
     return;
 }
